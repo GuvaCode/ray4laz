6f28a0677a0fdfb1e1580db439768ebed0391df6
Vadim <guvacode@gmail.com>
2022-03-31 10:44:22 +0500

    sync with master branch

--- source/raylib.pas
+++ source/raylib.pas
@@ -348,10 +348,14 @@ const
      PrAudioBuffer = ^TrAudioBuffer;
      TrAudioBuffer = record end;
 
+     PrAudioProcessor = ^TrAudioProcessor;
+     TrAudioProcessor = record end;
+
      (* AudioStream, custom audio stream *)
      PAudioStream = ^TAudioStream;
      TAudioStream = record
-         buffer     : PrAudioBuffer; // Pointer to internal data used by the audio system
+         buffer     : PrAudioBuffer;    // Pointer to internal data used by the audio system
+         processor  : PrAudioProcessor; // Pointer to internal data processor, useful for audio effects
          sampleRate : dword;         // Frequency (samples per second)
          sampleSize : dword;         // Bit depth (bits per sample): 8, 16, 32 (24 not supported)
          channels   : dword;         // Number of channels (1-mono, 2-stereo, ...)
@@ -1431,7 +1435,7 @@ function CheckCollisionBoxes(box1:TBoundingBox; box2:TBoundingBox):boolean;cdecl
 function CheckCollisionBoxSphere(box:TBoundingBox; center:TVector3; radius:single):boolean;cdecl;external cDllName;// Check collision between box and sphere
 function GetRayCollisionSphere(ray:TRay; center:TVector3; radius:single):TRayCollision;cdecl;external cDllName;// Get collision info between ray and sphere
 function GetRayCollisionBox(ray:TRay; box:TBoundingBox):TRayCollision;cdecl;external cdllName;// Get collision info between ray and box
-function GetRayCollisionModel(ray:TRay; model:TModel):TRayCollision;cdecl;external cdllName;// Get collision info between ray and model
+// хз почему удалили function GetRayCollisionModel(ray:TRay; model:TModel):TRayCollision;cdecl;external cdllName;// Get collision info between ray and model
 function GetRayCollisionMesh(ray:TRay; mesh:TMesh; transform:TMatrix):TRayCollision;cdecl;external cdllName;// Get collision info between ray and mesh
 function GetRayCollisionTriangle(ray:TRay; p1:TVector3; p2:TVector3; p3:TVector3):TRayCollision;cdecl;external cDllName;// Get collision info between ray and triangle
 function GetRayCollisionQuad(ray:TRay; p1:TVector3; p2:TVector3; p3:TVector3; p4:TVector3):TRayCollision;cdecl;external cDllName;// Get collision info between ray and quad
@@ -1441,6 +1445,7 @@ function GetRayCollisionQuad(ray:TRay; p1:TVector3; p2:TVector3; p3:TVector3; p4
 //------------------------------------------------------------------------------------
 PAudioCallback = ^TAudioCallback;
 TAudioCallback = procedure (bufferData: pointer; frames: dword);cdecl;
+//typedef void (*AudioCallback)(void *bufferData, unsigned int frames);
 
 (* Audio device management functions *)
 procedure InitAudioDevice;cdecl;external cDllName;// Initialize audio device and context
@@ -1508,7 +1513,10 @@ procedure SetAudioStreamVolume(stream:TAudioStream; volume:single);cdecl;externa
 procedure SetAudioStreamPitch(stream:TAudioStream; pitch:single);cdecl;external cDllName;//  Set pitch for audio stream (1.0 is base level)
 procedure SetAudioStreamPan(stream:TAudioStream; pan:single);cdecl;external cDllName;// Set pan for audio stream (0.5 is centered)
 procedure SetAudioStreamBufferSizeDefault(size:longint);cdecl;external cDllName;//  Default size for new audio streams
-procedure SetAudioStreamCallback(stream:AudioStream; callback:TAudioCallback);cdecl;external cDllName;// Audio thread callback to request new data
+procedure SetAudioStreamCallback(stream:TAudioStream; callback:TAudioCallback);cdecl;external cDllName;// Audio thread callback to request new data
+procedure AttachAudioStreamProcessor(stream: TAudioStream; processor: TAudioCallback);cdecl;external cDllName;
+procedure DetachAudioStreamProcessor(stream: TAudioStream; processor: TAudioCallback);cdecl;external cDllName;
+
 
 // Custom Misc Functions to help simplify a few things
 function Vector2Create(aX: single; aY: single): TVector2;
