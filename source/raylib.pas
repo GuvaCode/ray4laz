ccf491e9092d1516b129bed8db71091c641587f6
Gunko Vadim <guvacode@gmail.com>
2022-09-28 23:42:31 +0500

    Update raylib.pas

--- source/raylib.pas
+++ source/raylib.pas
@@ -587,7 +587,7 @@ const
          MOUSE_BUTTON_MIDDLE   = 2; // Mouse button middle (pressed wheel)
          MOUSE_BUTTON_SIDE     = 3; // Mouse button side (advanced mouse device)
          MOUSE_BUTTON_EXTRA    = 4; // Mouse button extra (advanced mouse device)
-         MOUSE_BUTTON_FORWARD  = 5; // Mouse button fordward (advanced mouse device)
+         MOUSE_BUTTON_FORWARD  = 5; // Mouse button forward (advanced mouse device)
          MOUSE_BUTTON_BACK     = 6; // Mouse button back (advanced mouse device)
 
          (* Add backwards compatibility support for deprecated names *)
