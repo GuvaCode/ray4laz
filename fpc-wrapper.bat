ed87b0fb5503036b740676b6fa34100c94ba5b56
Matthias Grandis <contact@mattgrandis.net>
2024-04-18 14:31:46 +0200

    Update fpc-wrapper.bat
    Forget to remove -Twin64 and -Px86_64 from default flags

--- fpc-wrapper.bat
+++ fpc-wrapper.bat
@@ -15,7 +15,7 @@ REM Remove trailing backslash
 SET "HERE=%HERE:~0,-1%"
 
 REM Set base flags and include flags
-SET BASE_FLAGS=-MObjFPC -Scghi -Cg -l -vewnhibq -WG -Twin64 -Px86_64
+SET BASE_FLAGS=-MObjFPC -Scghi -Cg -l -vewnhibq
 SET INCLUDE_FLAGS=-Fl"%HERE%\libs\%PLATFORM%" -Fu"%HERE%\source" -Fu"%HERE%\headers"
 
 REM Set platform default
@@ -24,7 +24,7 @@ SET PLATFORM=x86_32-windows
 REM Check the architecture
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
 	SET PLATFORM=x86_64-windows
-    SET BASE_FLAGS=%BASE_FLAGS% -Twin64 -Px86_64
+	SET BASE_FLAGS=%BASE_FLAGS% -Twin64 -Px86_64
 )
 
 REM Run the fpc command
